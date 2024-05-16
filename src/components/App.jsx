a2fe4bf1b5c6e9864fb92a31ff29970be0316dd6
Marianna Dei <d.marianna@icloud.com>
2024-05-17 00:59:23 +0300

    Added code to files App, Description, Options, Feedback,  Notifacation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,27 @@
+import { Component } from 'react';
+import Description from './Description/Description';
+import Options from './Options/Options';
+import Feedback from './Feedback/Feedback';
+// import Notification from './Notification/Notification';
 
+class App extends Component {
+  state = {
+    feedback: {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    },
+  };
+
+  render() {
+    return (
+      <div>
+        <Description />
+        <Options />
+        <Feedback options={this.state.feedback} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,1 +1,15 @@
+const Feedback = ({ good, neutral, bad, total, positivePerc }) => {
+  return (
+    <>
+      <div>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive: {positivePerc}%</p>
+      </div>
+    </>
+  );
+};
 
+export default Feedback;
